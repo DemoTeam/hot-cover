12f97ec427f00b12133adccfe419b3bc162d65ef
Hoang Quan <hoang.van.quan@framgia.com>
2015-01-19 16:49:07 +0700

    create show and edit post for admin

--- app/views/admins/posts/show.blade.php
+++ app/views/admins/posts/show.blade.php
@@ -1,105 +1,51 @@
-@extends('layouts.visitor')
+@extends('layouts.admin')
 @section('content')
 <!-- reservation-information -->
-<div id="information" class="spacer reserve-info ">
-<div class="container">
-<div class="row">
-<div class="col-sm-7 col-md-8">
-  <div><h2>{{ $post->title}}</h2></div>
-    <iframe  src="{{ViewHelper::convertUrl($post->content)}}" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-<hr>
-<!-- like -->
-<div class="rateWrapper"><span class="like rate rateUp" id="{{$post->id}}" data-item="{{$post->id}}">
-<span class="rateUpN">{{$post->true_likes()->count()}}</span></span>
-<span class="disLike rate rateDown" data-item="{{$post->id}}"><span class="rateDownN">{{$post->disLikes()->count()}}</span></span></div>
-<!-- end like -->
-<div>{{ $post->title}}</div>
-<div>{{ $post->description}}</div>
-</div>
-<div class="col-sm-5 col-md-4">
-<h3>Related video</h3>
-  @foreach($posts as $p)
-    <div class="wowload fadeInRight">
-	<iframe  class="embed-responsive-item" src="{{ViewHelper::convertUrl($post->content)}}" width="200px" height="150" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>		
-	<div>{{ link_to_route('posts.show', $p->title, array($p->id)) }}</div>
-    </div>    
-  @endforeach
-</div>
-</div>  
-</div>
-</div>
-<!-- reservation-information -->
-
-<h4>reservation-information</h4>
-<hr>
-
-<!-- services -->
-<div class="spacer services wowload fadeInUp">
-<div class="container">
-    <div class="row">
-        <div class="col-sm-4">
-            <!-- RoomCarousel -->
-            <div id="RoomCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                <div class="item active"><img src="{{ asset('images/photos/1.jpg') }}" class="img-responsive" alt="slide"></div>                
-                <div class="item  height-full"><img src="{{ asset('images/photos/2.jpg') }}"  class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="{{ asset('images/photos/3.jpg') }}"  class="img-responsive" alt="slide"></div>
-                </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#RoomCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                <a class="right carousel-control" href="#RoomCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
-            </div>
-            <!-- RoomCarousel-->
-            <div class="caption"><a href="rooms-tariff.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
-        </div>
-
-
-        <div class="col-sm-4">
-            <!-- RoomCarousel -->
-            <div id="RoomCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                <div class="item active"><img src="{{ asset('images/photos/4.jpg') }}" class="img-responsive" alt="slide"></div>                
-                <div class="item  height-full"><img src="{{ asset('images/photos/5.jpg') }}"  class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="{{ asset('images/photos/6.jpg') }}"  class="img-responsive" alt="slide"></div>
-                </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#RoomCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                <a class="right carousel-control" href="#RoomCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
-            </div>
-            <!-- RoomCarousel-->
-            <div class="caption"><a href="rooms-tariff.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
-        </div>
-
-
-        <div class="col-sm-4">
-            <!-- RoomCarousel -->
-            <div id="RoomCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                <div class="item active"><img src="{{ asset('images/photos/8.jpg') }}" class="img-responsive" alt="slide"></div>                
-                <div class="item  height-full"><img src="{{ asset('images/photos/9.jpg') }}"  class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="{{ asset('images/photos/10.jpg') }}"  class="img-responsive" alt="slide"></div>
-                </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#RoomCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                <a class="right carousel-control" href="#RoomCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
-            </div>
-            <!-- RoomCarousel-->
-            <div class="caption"><a href="rooms-tariff.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
-        </div>
-    </div>
-</div>
-</div>
-<script type="text/javascript">
-    $('#input-rating').on('rating.change', function(event, value, caption) {
-        $.ajax({
-            type: 'PUT',
-            url: '{{ URL::action("PostController@update", [$post->id]) }}',
-            dataType:'JSON',
-            data: {rate: value},
-            success: function(data){
-              alert("success");
-            }
-        });
-    });
-</script>
+    <table class="table table-bordered">
+      <tr>
+        <th>Title</th>
+        <td colspan="2"><b>{{ $post->title}}</b></td>
+      </tr>
+      <tr>
+        <th>Type</th>
+        <td colspan="2"><div class="btn {{$post->type=='adult' ? 'btn-danger glyphicon glyphicon-ban-circle' : 'btn-info'}}">
+         {{ $post->type }}</div></td>
+      </tr>
+      <tr>
+        <th>Category</th>
+        <td colspan="2"><div class="btn btn-info glyphicon {{$post->category == 'photo' ? 'glyphicon-picture' : 'glyphicon-facetime-video' }}">
+         {{ $post->category }}</div></td>
+      </tr>
+      <tr>
+        <th>Status</th>
+        <td colspan="2"><div class="btn {{$post->status == 'New' ? 'btn-warning' : ''}} {{$post->status == 'Approve' ? 'btn-info' : ''}} {{$post->status == 'Decline' ? 'btn-danger' : ''}}">
+         {{ $post->status }}</div></td>
+      </tr>
+      <tr>
+        <th>Content</th>
+        <td colspan="2">
+          @if($post->category == "photo")
+            {{ ViewHelper::displaySmallPhoto($post->content)  }}
+          @elseif($post->category == "video")
+            <iframe align="center" style="width:90%; height:350px"  src="{{ViewHelper::convertUrl($post->content)}}"  
+             frameborder="yes" scrolling="yes" name="myIframe" id="myIframe"> </iframe>
+          @endif
+        </td>
+      </tr>
+      <tr>
+        <th>Description</th>
+        <td colspan="2">{{ $post->description}}</td>
+      </tr>
+      <tr>
+        <th></th>
+        <td>
+          {{ link_to_route('admin.posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-info')) }}
+        </td>
+        <td>
+          {{ Form::open(array('method' => 'DELETE', 'route' => array('admin.posts.destroy', $post->id))) }} 
+          {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }} 
+          {{ Form::close() }}
+        </td>
+      </tr>
+    </table>  
 @stop
